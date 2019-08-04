07829e5806fa073071ccf20e988c82c948d009cc
Vriyas Hartama <vriyashartama@gmail.com>
2019-08-04 19:01:09 +0700

    Add auth app addition

--- regsys_api/authsys/urls.py
+++ regsys_api/authsys/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenRefreshView
+)
 from .views import (
     GetUser, RegisterView, LoginView, RegistrationConfirmationView,
     ForgotPasswordHandlerView, ResetPasswordHandler
@@ -8,6 +11,7 @@ urlpatterns = [
     path('', GetUser.as_view(), name='get_details'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('confirm/', RegistrationConfirmationView.as_view(), name='confirm_email'),
     path('reset/confirm/', ResetPasswordHandler.as_view(),
          name='reset_password_token'),
