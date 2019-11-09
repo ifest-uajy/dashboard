70f1535784f7ea6bc2494158703b025bee62e5ce
Vriyas Hartama <vriyashartama@gmail.com>
2019-11-09 21:52:53 +0700

    change to django

--- regsys_api/settings.py
+++ regsys_api/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'regsys_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
@@ -96,8 +96,9 @@ DATABASES = {
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
-}"""
+}
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -107,7 +108,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432' 
     }
-}
+}"""
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
